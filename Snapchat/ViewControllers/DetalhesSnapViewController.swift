7ed003a45330fbfa71faa44d24c4a30fa7a1b41b
Matheus Rodrigues Araujo <matheusaraujo@NBBV016619.BRQ.COM>
2020-01-21 12:00:13 -0300

    Subindo app finalizado

---
+++ Snapchat/ViewControllers/DetalhesSnapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetalhesSnapViewController.swift
+//  Snapchat
+//
+//  Created by Matheus Rodrigues Araujo on 21/01/20.
+//  Copyright © 2020 Curso IOS. All rights reserved.
+//
+
+import UIKit
+
+class DetalhesSnapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
